1f0622fcd2ed2640740f7661ff75b91aa058461f
Nick-Rimmer2016 <nick.rimmer@ndr-itsolutions.co.uk>
2017-05-24 21:00:35 +0100

    Create TEST.ps1

---
+++ TEST.ps1
@@ -0,0 +1,21 @@
+Configuration webservice
+{
+    param ($MachineName)
+        Node $MachineName
+        {
+                #Install IIS Role
+                WindowsFeature IIS
+                {
+                    Ensure = "Present"
+                    Name = "Web-Server"
+                }
+                #Install ASP.NET 4.5
+                WindowsFeature ASP
+                {
+                    Ensure = "Present"
+                    Name = "web-Asp-Net45"
+                }
+        }
+}
+
+webservice -MachineName localhost
