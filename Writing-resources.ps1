beb2682146d7cb4b45e3ae38ee6af18128ae1870
Nick-Rimmer2016 <nick.rimmer@ndr-itsolutions.co.uk>
2017-06-23 00:38:28 +0100

    Create Writing-resources.ps1

---
+++ Writing-resources.ps1
@@ -0,0 +1,14 @@
+$source = New-xDscResourceProperty –Name Source -Type String -Attribute Key
+$log = New-xDscResourceProperty –Name Log -Type String
+$Ensure = New-xDscResourceProperty –Name Ensure -Type String -Attribute Write –ValidateSet “Present”, “Absent”
+
+
+New-xDscResource –Name CheckEVLSource –Property $source, $Ensure,$log `
+–Path 'C:\Program Files\WindowsPowerShell\Modules' –ModuleName CheckEVLSource
+
+psedit "C:\Program Files\WindowsPowerShell\Modules\CheckEVLSource\DSCResources\CheckEVLSource\CheckEVLSource.psm1"
+
+$source = "TEST"
+[System.Diagnostics.EventLog]::SourceExists($source)
+New-EventLog   -LogName Application -Source AGDSC
+Remove-EventLog -source TEST -Verbose
