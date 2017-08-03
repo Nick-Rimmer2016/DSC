23def4b3dcb0f37cdb2df2c22a3552c925b4610c
Nick-Rimmer2016 <nick.rimmer@ndr-itsolutions.co.uk>
2017-08-03 10:47:36 +0100

    Update Passwords.ps1

--- Passwords.ps1
+++ Passwords.ps1
@@ -1,6 +1,10 @@
-$key = (2,3,56,34,254,222,1,1,2,23,42,54,33,233,1,34,2,7,6,5,35,43,6,6,6,6,6,6,31,33,60,23)
-ConvertTo-SecureString "lsfjsldj034503485034"  -AsPlainText -Force | ConvertFrom-SecureString -key $key | Set-Content E:\pass.txt
+$key = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($key)
+Set-Content E:\lability\test.txt $key
+$key = get-Content e:\lability\test.txt
+ConvertTo-SecureString "lsfjsldj034503485034"  -AsPlainText -Force | ConvertFrom-SecureString -key $key | Set-Content E:\lability\pass.txt
 
-$securestring = Get-Content E:\pass.txt | ConvertTo-SecureString -key $key
+$key = get-Content e:\lability\test.txt
+$securestring = Get-Content E:\lability\pass.txt | ConvertTo-SecureString -key $key
 $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($securestring))
 $password
